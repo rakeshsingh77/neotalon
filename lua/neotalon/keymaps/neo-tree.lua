3a0c039f56e520cd2d578e51ebf65a501d4177d0
Rakesh Singh <rakesh@sysras.com>
2026-01-15 13:22:01 +0200

    change neotree keybindings

--- lua/neotalon/keymaps/neo-tree.lua
+++ lua/neotalon/keymaps/neo-tree.lua
@@ -1,9 +1,9 @@
 local wk = require("which-key")
 
 wk.add({
-	{ "<c-n>", name = "NeoTree" },
-	{ "<c-n>e", "<cmd>Neotree toggle<cr>", desc = "Explorer" },
-	{ "<c-n>f", "<cmd>Neotree float<cr>", desc = "Float" },
-	{ "<c-n>w", "<cmd>Neotree focus<cr>", desc = "Focus" },
-	{ "<c-n>r", "<cmd>Neotree reveal<cr>", desc = "Reveal" },
+	{ "<leader>n", name = "NeoTree" },
+	{ "<leader>ne", "<cmd>Neotree toggle<cr>", desc = "Explorer" },
+	{ "<leader>nf", "<cmd>Neotree float<cr>", desc = "Float" },
+	{ "<leader>ns", "<cmd>Neotree focus<cr>", desc = "Focus" },
+	{ "<leader>nr", "<cmd>Neotree reveal<cr>", desc = "Reveal" },
 })
